81902caf7e2414fc2c1f6695e25ca3b8cdb61797
Spencer Ueda <iwamuraspencer@gmail.com>
2017-04-03 07:10:10 +0000

    renew files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
+  
   def show # 追加
-   @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
   
   def new
@@ -12,16 +13,36 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user # ここを修正
+      redirect_to @user
     else
-      render 'new'
+      render 'edit'
     end
   end
-
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Updated your Profile"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
-
+  
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :location, 
+    :password,
                                  :password_confirmation)
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path if @user != current_user
   end
 end
